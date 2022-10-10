77a993b99de969e188b8be169b8c231eddecd30e
TinTooth <boconne4@gmail.com>
2022-10-10 12:49:26 -0500

    created super and super_type models and created serializer files

--- supers/models.py
+++ supers/models.py
@@ -1,3 +1,13 @@
+from tkinter import CASCADE
+from super_types.models import SuperType
 from django.db import models
 
 # Create your models here.
+class Supers(models.Model):
+    name = models.CharField(max_length = 255)
+    alter_ego = models.CharField(max_length = 255)
+    primary_ability = models.CharField(max_length = 255)
+    secondary_ability = models.CharField(max_length = 255)
+    super_typ = models.ForeignKey(SuperType, on_delete = CASCADE)
+
+    
